30e596dce49620b1a2e444feeb62a89889d04d79
Rhea <technogal@pm.me>
2026-04-24 18:11:43 -0600

    added assets and dependencies

---
+++ images.g.ts
@@ -0,0 +1,69 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "hammerDummy":return img`
+. 5 5 . 
+5 5 5 5 
+5 5 5 5 
+. 5 5 . 
+`;
+            case "image2":
+            case "hammerBotDown":return img`
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . f f f f f 6 6 f f f f f . . 
+. f f 4 4 f f 6 6 f f 4 4 f f . 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 f f f 6 6 f f f 4 f 6 6 
+6 6 f f f f f . . f f f f f 6 6 
+6 6 f f f f . . . . f f f f 6 6 
+. f f f f . . . . . . f f f f . 
+`;
+            case "image3":
+            case "myImage":return img`
+. . f f f f f f f f f f f f . . 
+. f f 4 4 f f f f f f 4 4 f f . 
+6 6 f 4 4 f f f f f f 4 4 f 6 6 
+6 6 f 4 4 f f f f f f 4 4 f 6 6 
+6 6 f 4 f f f 6 6 f f f 4 f 6 6 
+6 6 f f f f f 6 6 f f f f f 6 6 
+6 6 f f f f . 6 6 . f f f f 6 6 
+. f f f f . . 6 6 . . f f f f . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
