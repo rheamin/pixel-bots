08496c2edcc6c83e549e9b9ed05122ac0fbaceb2
Rhea <technogal@pm.me>
2026-04-24 20:44:40 -0600

    Restore '2026-04-24, 6:11:43 p.m. added assets and dependencies'

--- images.g.ts
+++ images.g.ts
@@ -11,42 +11,34 @@ namespace myImages {
 . 5 5 . 
 `;
             case "image2":
-            case "hammerBot":return img`
-. . . . . . 6 6 6 6 6 . 
-. . . . . f 6 6 6 6 6 f 
-. . . . f f f f f f f f 
-. . . . f 4 4 4 4 f f f 
-. . . . f 4 4 4 f f f f 
-. . . . f f f f f f f . 
-. 2 2 . f f f f f f . . 
-2 2 2 2 6 6 6 6 6 . . . 
-2 2 2 2 6 6 6 6 6 . . . 
-. 2 2 . f f f f f f . . 
-. . . . f f f f f f f . 
-. . . . f 4 4 4 f f f f 
-. . . . f 4 4 4 4 f f f 
-. . . . f f f f f f f f 
-. . . . . f 6 6 6 6 6 f 
-. . . . . . 6 6 6 6 6 . 
+            case "hammerBotDown":return img`
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
+. . f f f f f 6 6 f f f f f . . 
+. f f 4 4 f f 6 6 f f 4 4 f f . 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 4 f f 6 6 f f 4 4 f 6 6 
+6 6 f 4 f f f 6 6 f f f 4 f 6 6 
+6 6 f f f f f . . f f f f f 6 6 
+6 6 f f f f . . . . f f f f 6 6 
+. f f f f . . . . . . f f f f . 
 `;
             case "image3":
-            case "hammerBotAttack":return img`
-. . 6 6 6 6 6 . . . . . 
-. f 6 6 6 6 6 f . . . . 
-f f f f f f f f . . . . 
-f 4 4 4 4 f f f . . . . 
-f 4 4 4 f f f f . . . . 
-f f f f f f f . . . . . 
-f f f f f f . . . 2 2 . 
-f f f f 6 6 6 6 2 2 2 2 
-f f f f 6 6 6 6 2 2 2 2 
-f f f f f f . . . 2 2 . 
-f f f f f f f . . . . . 
-f 4 4 4 f f f f . . . . 
-f 4 4 4 4 f f f . . . . 
-f f f f f f f f . . . . 
-. f 6 6 6 6 6 f . . . . 
-. . 6 6 6 6 6 . . . . . 
+            case "myImage":return img`
+. . f f f f f f f f f f f f . . 
+. f f 4 4 f f f f f f 4 4 f f . 
+6 6 f 4 4 f f f f f f 4 4 f 6 6 
+6 6 f 4 4 f f f f f f 4 4 f 6 6 
+6 6 f 4 f f f 6 6 f f f 4 f 6 6 
+6 6 f f f f f 6 6 f f f f f 6 6 
+6 6 f f f f . 6 6 . f f f f 6 6 
+. f f f f . . 6 6 . . f f f f . 
+. . . . . . . 2 2 . . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . 2 2 2 2 . . . . . . 
+. . . . . . . 2 2 . . . . . . . 
 `;
         }
         return null;
